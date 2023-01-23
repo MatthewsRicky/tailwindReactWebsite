34e995cfa9696fdbefde234b35f689508666c8ba
RickyMatthews <matthewsrickypro@gmail.com>
2023-01-23 22:54:15 +0300

    media assets

--- My-Gallery/src/components/Hero.jsx
+++ My-Gallery/src/components/Hero.jsx
@@ -9,13 +9,14 @@ function Hero() {
         <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-6">Grow with data
         </h1>
         <div className='flex justify-center items-center'>
-          <p className='md:text-5xl sm:4xl text-xl font-bold'>Fast, flexible financing for</p>
-          <Typed className='md:text-5xl sm:4xl text-xl font-bold text-[#00df9a] pl-2' strings={['BTB', 'BTC', 'SAAS']} typeSpeed={120} backspeed={140} loop />
+          <p className='md:text-5xl sm:4xl text-xl font-bold py-4'>Fast, flexible financing for</p>
+          <Typed className='md:text-5xl sm:4xl text-xl font-bold text-[#00df9a] md:pl-4 pl-2' strings={['BTB', 'BTC', 'SAAS']} typeSpeed={120} backspeed={140} loop />
 
         </div>
-        <p className='md:text-2xl sm:1xl text-xl font-bold pt-8 px- 4'>
+        <p className='md:text-2xl sm:1xl text-xl font-bold pt-8 px-4 text-gray-500'>
           Moniter your data analytics to increase revenue for BTB, BTC & SAAS platforms
         </p>
+        <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black hover:bg-lime-100'>Get Started</button>
       </div>
 
     </div>
